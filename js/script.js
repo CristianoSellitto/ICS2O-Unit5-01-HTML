1f988cded4de306dce3e63092d224c226497d97d
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-20 17:24:52 +0000

    Added program

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 // Cristiano
-// ICS2O-Unit0-00-HTML
-// Date 2022
+// ICS2O-Unit5-01-HTML
+// April 20 2022
 
 'use strict'
+
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/<RESPITORY>/sw.js", {
-    scope: "/<RESPITORY>/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-01-HTML/",
   })
 }
+
+var randomnumber = Math.floor(Math.random() * 6) + 1;
+var number = 1
 
 /**
  * Alerts "Hello, World!"
  */
 function onButtonClick() {
-  alert("Hello, World!")
+  if (number == randomnumber) {
+    document.getElementById("result").innerHTML = "<br><h5>The random number was " + randomnumber + "!</h5><br><h5>You got it right!</h5>"
+  } else {
+    document.getElementById("result").innerHTML = "<br><h5>The random number was " + randomnumber + "!</h5><br><h5>You got it wrong...</h5>"
+  }
+  randomnumber = Math.floor(Math.random() * 6) + 1;
+  console.log("The current random number is " + randomnumber + "!")
 }
+
+function updateNumber() {
+  number = document.getElementById("slider").value
+  document.getElementById("slider-number").innerHTML = "<h4>Selected Number: " + number + "</h4>"
+}
+
+console.log("The current random number is " + randomnumber + "!")
